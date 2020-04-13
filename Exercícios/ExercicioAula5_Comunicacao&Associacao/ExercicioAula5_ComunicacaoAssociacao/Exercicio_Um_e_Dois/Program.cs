dd6a9b5b4f037a6ff3feb229eebf1e570f48a5ab
Felipe <felipe-sartori@outlook.com>
2020-04-13 12:36:12 -0300

    Deixando o código mais limpo

--- Exercícios/ExercicioAula5_Comunicacao&Associacao/ExercicioAula5_ComunicacaoAssociacao/Exercicio_Um_e_Dois/Program.cs
+++ Exercícios/ExercicioAula5_Comunicacao&Associacao/ExercicioAula5_ComunicacaoAssociacao/Exercicio_Um_e_Dois/Program.cs
@@ -10,8 +10,7 @@ namespace Exercicio_Um_e_Dois
     {
         static void Main(string[] args)
         {
-            DadosClientes dados = new DadosClientes();
-            BancoClientes guardaCliente = new BancoClientes();
+            BancoClientes insereCliente = new BancoClientes();
             int op;
 
             do
@@ -38,17 +37,17 @@ namespace Exercicio_Um_e_Dois
                             Console.Clear();
                             Cliente cliente = new Cliente();
                             Endereco endereco = new Endereco();
-                            dados.PegaDados(cliente, endereco);
-                            guardaCliente.gravaCliente(cliente);
+                            pegaDadosCliente(cliente, endereco);
+                            insereCliente.gravaCliente(cliente);
                         }
                         break;
                     case 2:
                         {
                             Console.Clear();
                             Console.WriteLine("Clientes cadastrados: \n");
-                            for (int i = 0; i < guardaCliente.getQuantiaClientes(); i++)
+                            for (int i = 0; i < insereCliente.getQuantiaClientes(); i++)
                             {
-                                Console.WriteLine($"Cliente {i}: {guardaCliente.getCliente(i).Nome}\t CPF: {guardaCliente.getCliente(i).Cpf}\n");
+                                Console.WriteLine($"Cliente {i}: {insereCliente.getCliente(i).Nome}\t CPF: {insereCliente.getCliente(i).Cpf}\n");
                             }
                             Console.ReadKey();
                         }
@@ -58,11 +57,11 @@ namespace Exercicio_Um_e_Dois
                             Console.Clear();
                             Console.Write("Digite o CPF do cliente que quer consultar - Utilize o formato (XXX.XXX.XXX-XX): ");
                             String buscaCPF = Console.ReadLine();
-                            for (int i = 0; i < guardaCliente.getQuantiaClientes(); i++)
+                            for (int i = 0; i < insereCliente.getQuantiaClientes(); i++)
                             {
-                                if (buscaCPF == guardaCliente.getCliente(i).Cpf)
+                                if (buscaCPF == insereCliente.getCliente(i).Cpf)
                                 {
-                                    Console.WriteLine($"Nome: {guardaCliente.getCliente(i).Nome}\t CPF: {guardaCliente.getCliente(i).Cpf}\t Data de Nascimento: {guardaCliente.getCliente(i).DataNascimento}\t Idade: {guardaCliente.getCliente(i).Idade}\t Rua: {guardaCliente.getCliente(i).Endereco.NomeRua}\t Nº: {guardaCliente.getCliente(i).Endereco.Numero}\t Complemento: {guardaCliente.getCliente(i).Endereco.Complemento}\t CEP: {guardaCliente.getCliente(i).Endereco.Cep}\t Cidade: {guardaCliente.getCliente(i).Endereco.Cidade}\t UF: {guardaCliente.getCliente(i).Endereco.Uf}\n");
+                                    Console.WriteLine($"Nome: {insereCliente.getCliente(i).Nome}\t CPF: {insereCliente.getCliente(i).Cpf}\t Data de Nascimento: {insereCliente.getCliente(i).DataNascimento}\t Idade: {insereCliente.getCliente(i).Idade}\t Rua: {insereCliente.getCliente(i).Endereco.NomeRua}\t Nº: {insereCliente.getCliente(i).Endereco.Numero}\t Complemento: {insereCliente.getCliente(i).Endereco.Complemento}\t CEP: {insereCliente.getCliente(i).Endereco.Cep}\t Cidade: {insereCliente.getCliente(i).Endereco.Cidade}\t UF: {insereCliente.getCliente(i).Endereco.Uf}\n");
                                 }
                             }
                             Console.ReadKey();
@@ -73,7 +72,8 @@ namespace Exercicio_Um_e_Dois
                             Console.Clear();
                             Console.WriteLine("Digite o CPF do cliente que quer editar - Utilize o formato (XXX.XXX.XXX-XX): ");
                             String buscaCPF = Console.ReadLine();
-                            dados.EditaClientes(guardaCliente, buscaCPF);
+
+                            editaCliente(insereCliente, buscaCPF);
                         }
                         break;
                     default:
@@ -83,6 +83,61 @@ namespace Exercicio_Um_e_Dois
                         break;
                 }
             } while (op != 0);
+        }
+
+        private static void pegaDadosCliente(Cliente cliente, Endereco endereco)
+        {
+            Console.WriteLine("Para adicionar o cliente preencha com as informações: ");
+            Console.Write("Nome do cliente: ");
+            cliente.Nome = Console.ReadLine();
+            Console.Write("CPF do cliente (XXX.XXX.XXX-XX): ");
+            cliente.Cpf = Console.ReadLine();
+            Console.Write("Data de nascimento do cliente: ");
+            cliente.DataNascimento = Console.ReadLine();
+            Console.Write("Idade do cliente: ");
+            cliente.Idade = int.Parse(Console.ReadLine());
+            Console.Write("Nome da rua: ");
+            endereco.NomeRua = Console.ReadLine();
+            Console.Write("Número: ");
+            endereco.Numero = Console.ReadLine();
+            Console.Write("Complemento: ");
+            endereco.Complemento = Console.ReadLine();
+            Console.Write("CEP: ");
+            endereco.Cep = Console.ReadLine();
+            Console.Write("Cidade: ");
+            endereco.Cidade = Console.ReadLine();
+            Console.Write("UF: ");
+            endereco.Uf = Console.ReadLine();
+            cliente.Endereco = endereco;
+        }
+        private static void editaCliente(BancoClientes insereCliente, String buscaCPF)
+        {
+            for (int i = 0; i < insereCliente.getQuantiaClientes(); i++)
+            {
+                if (buscaCPF == insereCliente.getCliente(i).Cpf)
+                {
+                    Console.Write("Nome do cliente: ");
+                    insereCliente.getCliente(i).Nome = Console.ReadLine();
+                    Console.Write("CPF do cliente (XXX.XXX.XXX-XX): ");
+                    insereCliente.getCliente(i).Cpf = Console.ReadLine();
+                    Console.Write("Data de nascimento do cliente: ");
+                    insereCliente.getCliente(i).DataNascimento = Console.ReadLine();
+                    Console.Write("Idade do cliente: ");
+                    insereCliente.getCliente(i).Idade = int.Parse(Console.ReadLine());
+                    Console.Write("Nome da rua: ");
+                    insereCliente.getCliente(i).Endereco.NomeRua = Console.ReadLine();
+                    Console.Write("Número: ");
+                    insereCliente.getCliente(i).Endereco.Numero = Console.ReadLine();
+                    Console.Write("Complemento: ");
+                    insereCliente.getCliente(i).Endereco.Complemento = Console.ReadLine();
+                    Console.Write("CEP: ");
+                    insereCliente.getCliente(i).Endereco.Cep = Console.ReadLine();
+                    Console.Write("Cidade: ");
+                    insereCliente.getCliente(i).Endereco.Cidade = Console.ReadLine();
+                    Console.Write("UF: ");
+                    insereCliente.getCliente(i).Endereco.Uf = Console.ReadLine();
+                }
+            }
         }
     }
 }
