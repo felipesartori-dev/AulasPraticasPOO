fc10810b8e144351f03a01eaa66718c3a9db1219
Felipe <felipe-sartori@outlook.com>
2020-04-14 20:00:14 -0300

    Adicionado nova aula

---
+++ Aulas/Aula6_MetodosVirtuais/Aula6_DeclarandoMetodosVirtuais/Exemplo1/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo1
+{
+    class Pessoa
+    {
+        public string nome { get; set; }
+        public int idade { get; set; }
+
+        public Pessoa(string nome, int idade)
+        {
+            this.nome = nome;
+            this.idade = idade;
+        }
+
+        public override string ToString()
+        {
+            return $"Pessoa: {nome}\nIdade: {idade}";
+        }
+    }
+}
---
+++ Aulas/Aula6_MetodosVirtuais/Aula6_DeclarandoMetodosVirtuais/Exemplo1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa("Felipe", 23);
+            Console.WriteLine(p);
+
+            Console.ReadKey();
+        }
+    }
+}
